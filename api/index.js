9ddbb97d9d6392187840e72f6552168c2c6e04e8
selam <selblessed@gmail.com>
2024-07-23 20:24:32 -0400

    setup mock-testing environment

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import axios from 'axios';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -12,11 +13,20 @@ mongoose.connect(process.env.MONGO)
 }
 );
 
-
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!!');
-}
-);
+//example api
+export const fetchData = async () => {
+  const response = await axios.get('https://api.example.com/data');
+  return response;
+};
+export default app;
+
+if (import.meta.url == `file://${process.argv[1]}`) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log('Server is running on port 3000!!');
+  }
+  );
+};
 
--- api/tests/api.test.js
+++ api/tests/api.test.js
@@ -1,4 +1,31 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
+import axios from 'axios';
+import { fetchData } from '../index'; 
+
+vi.mock('axios');
+
+describe('fetchData', () => {
+  it('should fetch data successfully', async () => {
+    const mockedResponse = { data: { message: 'Success' } };
+    axios.get.mockResolvedValueOnce(mockedResponse);
+
+    const result = await fetchData();
+
+    expect(result.data.message).toBe('Success');
+    expect(axios.get).toHaveBeenCalledWith('https://api.example.com/data');
+  });
+
+  it('should handle API error', async () => {
+    const errorMessage = 'Network Error';
+    axios.get.mockRejectedValueOnce(new Error(errorMessage));
+
+    try {
+      await fetchData();
+    } catch (error) {
+      expect(error.message).toBe(errorMessage);
+    }
+  });
+});
 
 describe('API Test', () => {
   it('should pass this test', () => {
