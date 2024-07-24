84fa37f8ded772e6d523ad8f30fd732fb8d376f3
Semhar Tes <semhartes7@gmail.com>
2024-07-23 23:33:32 -0400

    deploying to render

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO)
 }
 );
 
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -20,3 +22,8 @@ app.listen(3000, () => {
 }
 );
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
+});
