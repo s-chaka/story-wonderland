9be8705b4a513ec957e04c492ae0267bbcc64c3b
selam <selblessed@gmail.com>
2024-07-25 18:33:56 -0400

    create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-
-import TestComponent from "./TestComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import TestComponent from "./TestComponent";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 
 export default function App() {
   return (
     <div>
-      <h1 className='text-green-500'>Story Wonderland </h1>
-      <TestComponent />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/sign-in" element={<SignIn/>} />
+          <Route path="/profile" element={<Profile/>}/>
+          {/* <h1>Story Wonderland </h1>
+          <TestComponent /> */}
+        </Routes>        
+      </BrowserRouter>
     </div>
   );
 }
