a7da19feeaaf971b2dc60b5a7aa349c7c87cc3dc
selam <selblessed@gmail.com>
2024-07-29 13:18:09 -0400

    added google function under auth.controller, updated user.js to have the profilePic, added /google route in auth.routh.js, removed signup from Header and imported signInSuccess and used dispatch

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,15 +1,17 @@
 import { getAuth, GoogleAuthProvider , signInWithPopup} from 'firebase/auth';
 import { app } from '../firebase';
-// import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
-      console.log(result);
       const res = await fetch('/api/auth/google',{
         method: 'POST',
         headers: {
@@ -18,12 +20,12 @@ export default function OAuth() {
         body: JSON.stringify({
           name: result.user.displayName,
           email: result.user.email,
-          photo: result.user.photoURL
-
-        })
+          photo: result.user.photoURL,
+        }),
       });
       const data = await res.json();
-      // dispatch(signInSuccess(data));
+      console.log(data);
+      dispatch(signInSuccess(data));
     } catch (error) {
       console.log('unable to login with google',error);
     }
