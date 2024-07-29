8119719452faa87e12dcf5e99da558ffb37d6dc2
selam <selblessed@gmail.com>
2024-07-29 14:27:27 -0400

    create PrivateRoute component, wrap the profile with PrivateRoute and updated Header to show profile conditionally

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const {currentUser} = useSelector((state) => state.user);
   return (
     <div className='bg-green-200'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'> 
@@ -15,8 +16,13 @@ export default function Header() {
                 <Link to='/about'>
                 <li>About</li>
                 </Link>
-                <Link to='/Sign-in'>
-                <li>Sign In</li>
+                <Link to='/profile'>
+                  {currentUser ? (
+                    <img src={currentUser.profilePic}
+                    alt='profile' className='h-7 w-7 rounded-full object-cover'/>
+                  ) : (
+                      <li>Sign In</li>
+                  )}
                 </Link>
             </ul>
         </div>
