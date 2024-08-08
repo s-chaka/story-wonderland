abe79ff18c9c2ec0e7a7bf933107542420ee14c3
selam <selblessed@gmail.com>
2024-08-08 14:38:39 -0400

    removed body tag from home page to fix warning in the web

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,22 @@
 import BackgroundImage from './BackgroundImage.jsx';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <body className="h-screen">
+    <div className="h-screen">
       <div className="flex flex-col items-center justify-center h-full">
         <h1 className="text-red-300 text-center mb-4">
           Welcome to Story Wonderland!!📙🌀䷉ <br/> 
           A World of Creative Stories of All Kind!!
         </h1>
-        <a href="/sign-up" className="text-blue-500 hover:underline">
-          Signup here to get started!
-        </a>
+        <p className='text-white text-center' > Click here :-
+          <Link to="/sign-in" className="ml-2 mr-2 text-blue-500 hover:underline uppercase">
+            Signup/Signin 
+          </Link> to get started!
+        </p>
         <BackgroundImage />
       </div>
-    </body>
+    </div>
   );
 };
 
