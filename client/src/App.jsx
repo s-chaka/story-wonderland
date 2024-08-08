ae2cf2ab8cf051130df5521bfabd6c02ced6360f
selam <selblessed@gmail.com>
2024-08-07 20:18:28 -0400

    created a SignOut page and moved the signout function from profile page to Signout page and render it in the Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import Stories from "./pages/Stories";
+import SignOut from "./pages/SignOut";
 
 
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<SignIn/>} />
           <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/sign-out" element={<SignOut/>} />
           <Route path="/stories" element={<Stories/>} />
           <Route element={<PrivateRoute/>}>
             <Route path="/profile" element={<Profile/>}/>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 
 export default function Header() {
   const {currentUser} = useSelector((state) => state.user);
+
   return (
     <div className='bg-green-200'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'> 
@@ -11,19 +12,29 @@ export default function Header() {
             </Link>
             <ul className='flex gap-4'>
                 <Link to='/'>
-                <li>Home</li>
+                  <li>Home</li>
                 </Link>
                 <Link to='/about'>
-                <li>About</li>
+                  <li>About</li>
                 </Link>
-                <Link to='/profile'>
-                  {currentUser ? (
-                    <img src={currentUser.profilePic}
-                    alt='profile' className='h-7 w-7 rounded-full object-cover'/>
-                  ) : (
-                      <li>Sign In</li>
-                  )}
-                </Link>
+                {currentUser ? (
+                  <>
+                    <Link to='/sign-out'>
+                      <li className='cursor-pointer'>Sign Out</li>
+                    </Link>
+                    <Link to='/profile'>
+                      <img
+                        src={currentUser.profilePic}
+                        alt='profile'
+                        className='h-7 w-7 rounded-full object-cover'
+                      />
+                    </Link>
+                  </>
+                ) : (
+                  <Link to='/sign-in'>
+                    <li>Sign In</li>
+                  </Link>
+                )}
             </ul>
         </div>
     </div>
