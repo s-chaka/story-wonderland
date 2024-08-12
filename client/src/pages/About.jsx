4003ef209b8c82cac8a2ae35712d9aa96cd93080
selam <selblessed@gmail.com>
2024-08-12 19:20:08 -0400

    updated styles in Home and About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
   return (
+    <div className='min-h-screen bg-gradient-to-r from-purple-200 via-blue-200 to-red-200 bg-opacity-60 backdrop-filter backdrop-blur-lg flex items-center justify-center'>
     <div className='p-6 max-w-4xl mx-auto bg-white shadow-md rounded-lg mt-10'>
-      <h1 className='text-3xl font-bold text-center text-orange-400 mb-4 mt-8'>
+      <h1 className='text-3xl font-bold text-center text-purple-400 mb-4 mt-8'>
         About Story Wonderland App
       </h1>
       <p className='text-lg text-gray-700 mb-6 mt-10'>
@@ -12,16 +13,16 @@ const About = () => {
         that you can create based on your favorite genres. With the help of our friendly AI, 
         we bring your chosen adventures to life in a way that’s just for you.
       </p>
-      <h2 className='text-2xl font-bold text-center text-orange-400 mb-4 mt-8'>
+      <h2 className='text-2xl font-bold text-center text-purple-400 mb-4 mt-8'>
         How It Works
       </h2>
       <p className='text-lg text-gray-700 mb-6'>
           Creating your own story in Story-Wonderland is as easy as picking the types of stories you 
-        like—whether it’s fairy tales, space adventures, or anything in between. Once you’ve made 
-        your choice, our AI puts together a fun story for you to enjoy. You can save your favorite stories, 
+        like whether it’s fairy tales, space adventures, or anything in between. Once you’ve made 
+        your choice, our app puts together a fun story for you to enjoy. You can save your favorite stories, 
         explore different paths, and let your imagination run wild!
       </p>
-      <h2 className='text-2xl font-bold text-center text-orange-400 mb-4 mt-8'>
+      <h2 className='text-2xl font-bold text-center text-purple-400 mb-4 mt-8'>
         Our Mission
       </h2>
       <p className='text-lg text-gray-700 mb-6'>
@@ -29,13 +30,14 @@ const About = () => {
         and make reading and storytelling a fun experience for kids everywhere. Our app is designed to be 
         a safe and delightful place where young minds can explore and grow.
       </p>
-      <h2 className='text-2xl font-bold text-center text-orange-400 mb-4 mt-8'>
+      <h2 className='text-2xl font-bold text-center text-purple-400 mb-4 mt-8'>
         Technology Behind the Magic
       </h2>
       <p className='text-lg text-gray-700'>
-          Story-Wonderland uses the MERN stack and advanced AI to create personalized stories for each child. 
-        We’ve made sure that our app is easy to use and safe, so kids can focus on what matters most having fun!
+          Story-Wonderland uses the MERN stack and llama API to create personalized stories for each child. 
+        We’ve made sure that our app is easy to use, so kids can focus on what matters most having fun!
       </p>
+    </div>
     </div>
   );
 };
